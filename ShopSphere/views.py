c2fff524d181e802bba702dc090762e00cfca8e8
Oskar Kerr (student) <2912252K@student.gla.ac.uk>
2025-03-17 15:48:13 +0000

    imporved taskbar and removed cookies

--- ShopSphere/views.py
+++ ShopSphere/views.py
@@ -6,34 +6,11 @@ from ShopSphere.forms import CategoryForm
 from django.shortcuts import redirect
 from ShopSphere.forms import PageForm
 from django.urls import reverse
-from ShopSphere.forms import UserForm, UserProfileForm
+from ShopSphere.forms import UserForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
-# A helper method
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-
-# Updated the function definition
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
-    # If it's been more than a day since the last visit...
-    if (datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-        # Update the last visit cookie now that we have updated the count
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        # Set the last visit cookie
-        request.session['last_visit'] = last_visit_cookie
-        # Update/set the visits cookie
-    request.session['visits'] = visits
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -43,19 +20,13 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-
-    visitor_cookie_handler(request)
     
     response = render(request, 'ShopSphere/index.html', context=context_dict)
     return response
 
 
 def about(request):
-    context_dict = {}
-    visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
-
-    return render(request, 'ShopSphere/about.html', context=context_dict)
+    return render(request, 'ShopSphere/about.html')
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -153,10 +124,9 @@ def register(request):
     # Attempt to grab information from the raw form information.
     # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
 
         # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid():
         # Save the user's form data to the database.
             user = user_form.save()
 
@@ -164,42 +134,24 @@ def register(request):
             # Once hashed, we can update the user object.
             user.set_password(user.password)
             user.save()
-
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves,
-            # we set commit=False. This delays saving the model
-
-            # until we're ready to avoid integrity problems.
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and
-            #put it in the UserProfile model.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-    # Now we save the UserProfile model instance.
-            profile.save()
-
+           
     # Update our variable to indicate that the template
     # registration was successful.
             registered = True
         else:
             # Invalid form or forms - mistakes or something else?
             # Print problems to the terminal.
-            print(user_form.errors, profile_form.errors)
+            print(user_form.errors)
     else:
         # Not a HTTP POST, so we render our form using two ModelForm instances.
         # These forms will be blank, ready for user input.
         user_form = UserForm()
-        profile_form = UserProfileForm()
+    
 
     # Render the template depending on the context.
     return render(request,
             'ShopSphere/register.html',
             context = {'user_form': user_form,
-                        'profile_form': profile_form,
                         'registered': registered})
 
 def user_login(request):
